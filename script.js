48bf4a3a3c8269074b1c27020a73e6897fef7d54
PRAVEEN KUMAR <117474739+AchintyaG@users.noreply.github.com>
2023-07-05 13:56:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
 
 function palindrome(str){
-
+	var str1= new String{};
+	for(let i=str.length(); i>0;i--)
+		{
+			str1=str.CharAt(i);
+		}
+	if(str1==str)
+	{
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 module.exports = palindrome
