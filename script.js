533d870bfe4f6762cf42204b7b53c2b54278c2b2
PRAVEEN KUMAR <117474739+AchintyaG@users.noreply.github.com>
2023-07-05 13:59:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 
 function palindrome(str){
 	var str1= new String{};
-	for(let i=str.length(); i>0;i--)
+	for(let i=str.length; i>0;i--)
 		{
-			str1=str.CharAt(i);
+			str1=str.charAt(i);
 		}
 	if(str1==str)
 	{
